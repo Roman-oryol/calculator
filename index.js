15112c95df7c0bcf9f0cc756f0e84d2e3fc627ac
Roman Oryol <roman.oryol83@gmail.com>
2025-01-23 16:38:06 +0200

    feat: add logic to display input values and update current value

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+const numberButtons = document.querySelectorAll('.btn.number');
+const currentValueElement = document.querySelector('.current-value');
+
+const MAX_LENGTH = 10;
+const displayedValues = [];
+let currentValue = 0;
+
 let a;
 let b;
 let operator;
+
+const displayValue = (enteredValue) => {
+  const isMaxLength = displayedValues.length == MAX_LENGTH;
+  const hasDecimalPoint = displayedValues.includes('.') && enteredValue === '.';
+  const isLeadingZero =
+    displayedValues.length === 1 &&
+    displayedValues[0] === '0' &&
+    enteredValue === '0';
+
+  if (isMaxLength || hasDecimalPoint || isLeadingZero) {
+    return;
+  }
+
+  if (displayedValues.length === 0 && enteredValue === '.') {
+    displayedValues.push('0');
+  }
+
+  displayedValues.push(enteredValue);
+  currentValueElement.textContent = displayedValues.join('');
+};
+
+const updateCurrentValue = (enteredValues) => {
+  currentValue = parseFloat(enteredValues.join(''));
+};
+
+const handleEnterNumber = (evt) => {
+  const enteredValue = evt.target.dataset.value;
+
+  displayValue(enteredValue);
+  updateCurrentValue(displayedValues);
+};
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', handleEnterNumber);
+});
 
 const operations = {
   '+': (a, b) => a + b,
@@ -8,4 +50,5 @@ const operations = {
   '*': (a, b) => a * b,
   '/': (a, b) => a / b,
 };
+
 const operate = (operator, a, b) => operations[operator](a, b);
